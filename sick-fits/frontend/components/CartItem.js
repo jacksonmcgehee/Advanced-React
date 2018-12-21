a0ce161a6055d27d9f7ceae9dbf7b29a2c67f1f5
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-12-21 10:16:05 -0500

    Still working through

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
-// import RemoveFromCart from './RemoveFromCart';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
     padding: 1rem 0;
@@ -18,26 +18,35 @@ const CartItemStyles = styled.li`
     }
 `;
 
-const CartItem = ({ cartItem }) => (
-    <CartItemStyles>
-        <img 
-            width='100'
-            src={cartItem.item.image}
-            alt={cartItem.item.title}
-        />
-        <div className='cart-item-details'>
-            <h3>{cartItem.item.title}</h3>
-            <p>
-                {formatMoney(cartItem.item.price * cartItem.quantity)}
-                {' - '}
-                <em>
-                    {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-                </em>
-            </p>
-        </div>
-        {/* <RemoveFromCart id={cartItem.id} /> */}
-    </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+    if (!cartItem.item) 
+        return (
+            <CartItemStyles>
+                <p>This item has been removed from the store.</p>
+                <RemoveFromCart id={cartItem.id} />
+            </CartItemStyles>
+        )
+
+    return (
+        <CartItemStyles>
+            <img 
+                width='100'
+                src={cartItem.item.image}
+                alt={cartItem.item.title}
+            />
+            <div className='cart-item-details'>
+                <h3>{cartItem.item.title}</h3>
+                <p>
+                    {formatMoney(cartItem.item.price * cartItem.quantity)}
+                    {' - '}
+                    <em>
+                        {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+                    </em>
+                </p>
+            </div>
+            <RemoveFromCart id={cartItem.id} />
+        </CartItemStyles>
+    )};
 
 CartItem.propTypes = {
     cartItem: PropTypes.object.isRequired,
--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -1,31 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
 
-const DelButton = styled.button`
-    font-size: 3rem;
-    background: none;
-    border: 0;
-    &:hover {
-        color: ${props.theme.red};
-        cursor: pointer;
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation removeFromCart($id: ID!) {
+    removeFromCart(id: $id) {
+      id
     }
+  }
 `;
 
-class RemoveFromCart extends Component {
-    static propTypes = {
-        id: PropTypes.string.isRequired,
-    }
-    render() {
-        return (
-            <DelButton title="Delete Item">
-                &times;
-            </DelButton>
-        );
-    }
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${props => props.theme.red};
+    cursor: pointer;
+  }
+`;
+
+class RemoveFromCart extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+  };
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          },
+        }}
+      >
+        {(removeFromCart, { loading, error }) => (
+          <BigButton
+            disabled={loading}
+            onClick={() => {
+              removeFromCart().catch(err => alert(err.message));
+            }}
+            title="Delete Item"
+          >
+            &times;
+          </BigButton>
+        )}
+      </Mutation>
+    );
+  }
 }
 
 export default RemoveFromCart;
